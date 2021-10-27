27b10ccae7b0ca912479037327821a9791896ab0
Nadav Shalti <snadav12@gmail.com>
2021-10-27 15:54:49 +0300

    add user to DB

--- seeds.js
+++ seeds.js
@@ -1,18 +1,30 @@
 const Http = new XMLHttpRequest();
-const url='http://localhost:3000';
+const url = 'http://localhost:3000';
 Http.open("POST", url);
 Http.send();
 
 
 
-const user = {userEmail='s234@gmail.com', username='toleMaster', password='123456', fullName='fullName'}
+const user = {
+    userEmail: 's2234@gmail.com',
+    username: 'toleMaster',
+    password: '123456',
+    fullName: 'fullName'
+}
 
-async function postData(url = url, data = {user}) {
-    method: 'POST', 
-    headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-
+async function postData(url = url, data = { user }) {
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
 }
--- server.js
+++ server.js
@@ -3,32 +3,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
-const dbUrl =
-  "mongodb+srv://Toledanos:s318720034@cluster0.jd0ti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
+const user = {
+    userEmail: 's2234@gmail.com',
+    username: 'toleMaster',
+    password: '123456',
+    fullName: 'fullName'
+}
+
+const dbUrl =
+    "mongodb+srv://Toledanos:s318720034@cluster0.jd0ti.mongodb.net/Mangalit?retryWrites=true&w=majority";
 // Make sure you place body-parser before your CRUD handlers!/
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(dbUrl, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.once("open", (_) => {
-  console.log("Database connected:", dbUrl);
+    console.log("Database connected:", dbUrl);
+    db.collection('Users').insertOne(user); //add user to DB
 });
 
 db.on("error", (err) => {
-  console.error("connection error:", err);
+    console.error("connection error:", err);
 });
 
 //CRUD Handlers.
 app.get("/", (req, res) => {
-  res.send("Wellwou World");
-  //res.sendFile(__Dirname + './index.html')
+    res.send("Wellwou World");
+    //res.sendFile(__Dirname + './index.html')
 });
 
 app.all("*", (req, res, next) => {
-  next(new ExpressError("Page Not Found", 404));
+    next(new ExpressError("Page Not Found", 404));
 });
 
-app.listen(3000, function () {
-  console.log("listening on port 3000");
+app.listen(3000, function() {
+    console.log("listening on port 3000");
 });
