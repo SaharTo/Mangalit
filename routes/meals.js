108ec854c9883495019895159b97508e5f938170
SaharTo <85928374+SaharTo@users.noreply.github.com>
2021-11-01 15:02:09 +0200

    added showMeal createMeal and deleteMeal functions to the mealController

--- routes/meals.js
+++ routes/meals.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const meals = require("../controllers/meals");
+//const meal = require("../models/meal");
 
-router.get("/", meals.index)
-
+router.get("/", meals.index);
+router.get("/mealws/:id", meals.mealById); //wrong route name on purpose
+router.get("/add", meals.createMeal);
+router.get("/delete", meals.deleteMeal);
 
 module.exports = router;
--- routes/sideMeals.js
+++ routes/sideMeals.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const sideMeals = require("../controllers/sideMeals");
 
-router.get("/", sideMeals.printSideMeals)
-router.get("/:id", sideMeals.sideMealById)
-router.post("/add", sideMeals.addSideMeal)
+router.get("/", sideMeals.printSideMeals);
+router.get("/:id", sideMeals.sideMealById);
+router.post("/add", sideMeals.addSideMeal);
 
 module.exports = router;
