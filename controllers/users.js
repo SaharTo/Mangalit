ed828febab5c2644dd166b9f90173d6df693e7f0
SaharTo <85928374+SaharTo@users.noreply.github.com>
2021-11-02 17:35:54 +0200

    add user functon in the controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,48 +1,48 @@
 const User = require("../models/user");
-const mongoose = require("mongoose");
 
 //functions that handle the render register and login pages an handle
 //the register, login and logout functions issues.
-const userModel = mongoose.model("User");
+//const userModel = mongoose.model("User");
 
-module.exports.printUser = async(req, res, next) => {
-    // const userM = new userModel();
-    // userM.username = 'Master www';
-    // userM.userEmail = 's111@gmail.com';
-    // userM.password = '111';
-    // userM.fullName = 'www';
-    // await userM.save();
+module.exports.printUser = async (req, res, next) => {
+  /* const userM = new userModel();
+   userM.username = 'Master www';
+   userM.userEmail = 's111@gmail.com';
+   userM.password = '111';
+   userM.fullName = 'www';
+   await userM.save();*/
 
-    User.find({}, function(err, users) {
-        res.send(users);
-    });
+  User.find({}, function (err, users) {
+    res.send(users);
+  });
 };
 
-module.exports.userById = async(req, res, next) => {
-    const user = await User.findById(req.params.id);
-    res.send(user);
-}
+module.exports.userById = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  res.send(user);
+};
 
-module.exports.register = async(req, res, next) => {
-    try {
-        const { userEmail, username, password, fullName } = req.body;
-        const user = new User({
-            userEmail,
-            username,
-            password,
-            fullName
-        });
-        await user.save();
-        /*const registeredUser = await User.register(user, password); 
+module.exports.register = async (req, res) => {
+  const user = new User();
+  user.userEmail = "Successfull";
+  user.userName = "successinguname";
+  user.password = "EasyPassword";
+  user.fullName = "mitzi awoooo";
+  // try {
+  //const { userEmail, username, password, fullName } = req.body;
+  await user.save();
+  res.send(user);
+
+  /*const registeredUser = await User.register(user, password); 
         It's about the that module "passportLocalMongoose" in the model.*/
-
-        /*req.login(registeredUser, (err) => {
+  /*req.login(registeredUser, (err) => {
           if (err) return next(err);
           req.flash("success", "Welcome to Yelp Camp!");
           res.redirect("/campgrounds");
         });*/
-    } catch (e) {
-        req.flash("error", e.message);
-        res.redirect("/register");
-    }
+  //} catch (e) {
+  //req.flash("error", e.message);
+  //  console.log(e);
+  // res.redirect("/users");
+  // }
 };
