b9061a4ff4a540627aa366335dc1afd1d8759c06
Nadav Shalti <snadav12@gmail.com>
2021-11-15 19:09:29 +0200

    add front sideMeals cmp, fetch data from backend

--- front/my-front/src/cmp/SideMeals/sideMeals.jsx
+++ front/my-front/src/cmp/SideMeals/sideMeals.jsx
@@ -1,11 +1,41 @@
+import './sideMeals.module.css';
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 
-// const Home = () => {
-//   return <div>This is the home page of the god dam Mangalit</div>;
-// };
-// export default Home;
+
 export class SideMeals extends Component {
+  state = {
+    sideMeals: null,
+  };
+  componentDidMount() {
+    this.getSideMeals();
+  }
+
+  getSideMeals = async () => {
+    fetch('http://localhost:3030/sideMeals')
+      .then(res => res.json())
+      .then(sideMeals => this.setState({ sideMeals }))
+      .catch(err => {
+        console.log(err);
+      })
+
+  };
+
   render() {
-    return <h1>SideMeals page</h1>;
+    const { sideMeals } = this.state;
+    if (!sideMeals) return <h1>Loading...</h1>;
+    return (
+      <div className="sideMeals-container">
+        {sideMeals.map((sideMeal) => (
+          <div className="sideMeal-preview" key={sideMeal._id}>
+            <h1>Name: {sideMeal.sideMealName}</h1>
+            <p>Summary: {sideMeal.sideMealSummary}</p>
+            <p>Price: {sideMeal.sideMealEstimatedPrice}₪</p>
+            {/* <img src='sideMeal.sideMealImageUrl' alt="img" /> */}
+            <Link to={'/sideMeals/' + sideMeal._id}>More Details</Link>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
--- server.js
+++ server.js
@@ -3,38 +3,47 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require('cors')
+
 const expressSession = require("express-session");
 
 const session = expressSession({
-  secret: "coding is amazing", //later we will take it from env file
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false },
+    secret: "coding is amazing", //later we will take it from env file
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
 });
 app.use(session);
 
 const dbUrl =
-  "mongodb+srv://Toledanos:s318720034@cluster0.jd0ti.mongodb.net/Mangalit?retryWrites=true&w=majority";
+    "mongodb+srv://Toledanos:s318720034@cluster0.jd0ti.mongodb.net/Mangalit?retryWrites=true&w=majority";
 // Make sure you place body-parser before your CRUD handlers!/
 app.use(express.json()); // To parse the incoming requests with JSON payloads
 
 app.use(express.urlencoded({ extended: true }));
 
+// for front accses
+const corsOptions = {
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+    credentials: true
+}
+app.use(cors(corsOptions))
+
 mongoose.connect(dbUrl, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.once("open", (_) => {
-  console.log("Database connected:", dbUrl);
-  // db.collection('Users').insertOne(user); //add user to DB
+    console.log("Database connected:", dbUrl);
+    // db.collection('Users').insertOne(user); //add user to DB
 });
 
 db.on("error", (err) => {
-  console.error("connection error:", err);
+    console.error("connection error:", err);
 });
 
 //CRUD Handlers.
 app.get("/", (req, res) => {
-  res.send("Wellwou World");
-  //res.sendFile(__Dirname + './index.html')
+    res.send("Wellwou World");
+    //res.sendFile(__Dirname + './index.html')
 });
 
 // app.all("*", (req, res, next) => {
@@ -46,6 +55,6 @@ app.use("/reviews/", require("./routes/review"));
 app.use("/meals/", require("./routes/meals"));
 app.use("/meats/", require("./routes/meats"));
 
-app.listen(3030, function () {
-  console.log("listening on port 3030");
+app.listen(3030, function() {
+    console.log("listening on port 3030");
 });
