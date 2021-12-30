ad59691c2785b473384a8f7287d56548cc2c1147
SaharTo <85928374+SaharTo@users.noreply.github.com>
2021-12-30 12:47:22 +0200

    test20

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //console.log("May Node be with you");
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -6,7 +7,7 @@ const cors = require("cors");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const dbUrl = process.env.DB_URL;
 mongoose.connect(dbUrl, { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -18,7 +19,7 @@ db.on("error", (err) => {
     console.error("connection error:", err);
 });
 const port = process.env.PORT || 3030;
-dotenv.config();
+
 // for front accses
 const corsOptions = {
     origin: [
@@ -29,18 +30,14 @@ const corsOptions = {
     credentials: true,
     methods: "GET, POST, PUT, DELETE",
 };
-// Make sure you place body-parser before your CRUD handlers!/
-app.use(express.json()); // To parse the incoming requests with JSON payloads
 
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     /*res.header(
-                                "Access-Control-Allow-Headers"
-                                "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-HTTP-Method-Override, Set-Cookie, Cookie"
-                            );*/
+                              "Access-Control-Allow-Headers"
+                              "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-HTTP-Method-Override, Set-Cookie, Cookie"
+                          );*/
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 });
@@ -67,6 +64,11 @@ app.use(
         cookie: { secure: false, httpOnly: false, maxAge: 24 * 360000 },
     })
 );
+// Make sure you place body-parser before your CRUD handlers!/
+app.use(express.json()); // To parse the incoming requests with JSON payloads
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 //CRUD Handlers.
 app.get("/", (req, res) => {
