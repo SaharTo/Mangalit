dced8166d7a1a0eba4decad9320d9b0d16914102
SaharTo <85928374+SaharTo@users.noreply.github.com>
2022-01-02 18:17:30 +0200

    fix some mobile issues

--- my-front/src/cmp/home/home.jsx
+++ my-front/src/cmp/home/home.jsx
@@ -17,28 +17,27 @@ export class Home extends Component {
         <h4 className={styles.subtitle}>לפני</h4>
         <h4 className={styles.subtitle}>שמכינים</h4>
         <h4 className={styles.subtitle}>בשר.</h4>
-        <div className={styles.recMeals} name='recMeals'>
-          <img className={styles.recImage} src="https://res.cloudinary.com/manglit/image/upload/v1640104952/assets/meat_wbube1.jpg" alt="steakImage" />
-          <Link to={"/recommendedMeals/"}>
+        <Link to={"/recommendedMeals/"}>
+          <div className={styles.recMeals} name='recMeals'>
+            <img className={styles.recImage} src="https://res.cloudinary.com/manglit/image/upload/v1640104952/assets/meat_wbube1.jpg" alt="steakImage" />
             <h2>לחץ/י כאן למנות מומלצות</h2>
-          </Link>
-          <p>10 המנות האהובות על משתמשי מנגלית</p>
-        </div>
-        <div className={styles.recSideMeals} name='recSideMeals'>
-          <img className={styles.recImage} src="https://res.cloudinary.com/manglit/image/upload/v1640104950/assets/fries_y3ot6h.jpg" alt="friesImage" />
-          <Link to={"/recommendedSideMeals/"}>
+            <p>10 המנות האהובות על משתמשי מנגלית</p>
+          </div>
+        </Link>
+        <Link to={"/recommendedSideMeals/"}>
+          <div className={styles.recSideMeals} name='recSideMeals'>
+            <img className={styles.recImage} src="https://res.cloudinary.com/manglit/image/upload/v1640104950/assets/fries_y3ot6h.jpg" alt="friesImage" />
             <h2>לחץ/י כאן למנות צד מומלצות</h2>
-          </Link>
-          <p>10 מנות הצד האהובות על משתמשי מנגלית</p>
-        </div>
-
-        <div className={styles.randomMeal} name='randomMeal'>
-          <img className={styles.recImage} src="https://res.cloudinary.com/manglit/image/upload/v1640104955/assets/random_zopmmo.jpg" alt="randomMeal" />
-          <Link to={"/roulette/"}>
+            <p>10 מנות הצד האהובות על משתמשי מנגלית</p>
+          </div>
+        </Link>
+        <Link to={"/roulette/"}>
+          <div className={styles.randomMeal} name='randomMeal'>
+            <img className={styles.recImage} src="https://res.cloudinary.com/manglit/image/upload/v1640104955/assets/random_zopmmo.jpg" alt="randomMeal" />
             <h2>לחץ/י כאן לבחירת מנה רנדומלית</h2>
-          </Link>
-          <p>בחירת מנה רנדומלית</p>
-        </div>
+            <p>בחירת מנה רנדומלית</p>
+          </div>
+        </Link>
 
       </div>
     );
