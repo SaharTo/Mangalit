4a71abe2a316e056030eae4792fd21e19e4e566a
SaharTo <85928374+SaharTo@users.noreply.github.com>
2022-01-09 13:42:23 +0200

    test

--- my-front/src/cmp/home/home.jsx
+++ my-front/src/cmp/home/home.jsx
@@ -20,7 +20,7 @@ export class Home extends Component {
         <Link to={"/recommendedMeals/"}>
           <div className={styles.recMeals} name='recMeals'>
             <img className={styles.recImage} src="https://res.cloudinary.com/manglit/image/upload/v1640104952/assets/meat_wbube1.jpg" alt="steakImage" />
-            <h2>לחץ/י כאן למנות מומלצות</h2>
+            <h2> נדב שלטייייי   לחץ/י כאן למנות מומלצות</h2>
             <p>10 המנות האהובות על משתמשי מנגלית</p>
           </div>
         </Link>
